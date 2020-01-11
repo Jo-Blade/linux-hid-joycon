073515930c9c41e27b6fabbc5c8b4d89bb391df7
Jo-Blade <59778661+Jo-Blade@users.noreply.github.com>
2020-01-11 22:46:43 +0100

    Add files via upload
    source code of the driver based of the existing driver of Daniel J. Ogorchock
    -> add the possibility to combined right and left joycons as one joystick

---
+++ hid-ids.h
@@ -0,0 +1,32 @@
+/*
+ *  USB HID quirks support for Linux
+ *
+ *  Copyright (c) 1999 Andreas Gal
+ *  Copyright (c) 2000-2005 Vojtech Pavlik <vojtech@suse.cz>
+ *  Copyright (c) 2005 Michael Haboustak <mike-@cinci.rr.com> for Concept2, Inc
+ *  Copyright (c) 2006-2007 Jiri Kosina
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ *version modifiee par Teo Pisenti
+ *
+ */
+
+#ifndef HID_IDS_H_FILE
+#define HID_IDS_H_FILE
+
+
+#define USB_VENDOR_ID_NINTENDO		0x057e
+#define USB_DEVICE_ID_NINTENDO_JOYCONL	0x2006
+#define USB_DEVICE_ID_NINTENDO_JOYCONR	0x2007
+#define USB_DEVICE_ID_NINTENDO_PROCON	0x2009
+
+
+
+
+#endif
